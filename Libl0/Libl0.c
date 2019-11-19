e57e972a9c5e628237b5fbe64d75e7ee37a050fe
laugh0608 <baihongjie0626@163.com>
2019-11-19 21:35:57 +0800

    233

--- Libl0/Libl0.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(){
-    FILE* fp = fopen("test.txt","w+");
-    char buf[1024] = "qidian-test";
-    fwrite(buf,strlen(buf),1,)
-}
